cc877e5ae10409928ae409bad43b952f6ce28851
Sergio Gomez <sergio.gomez2000@hotmail.com>
2024-04-25 11:53:41 -0500

    [FIX] routing in first login

--- apps/web/src/app/selectTeam/layout.tsx
+++ apps/web/src/app/selectTeam/layout.tsx
@@ -10,7 +10,7 @@ export default async function SelectTeamLayout({
 }>) {
   const userActiveTeam = await fetchQuery(api.user.getUserActiveTeam);
 
-  if (!userActiveTeam) {
+  if (userActiveTeam) {
     redirect("/loading");
   }
 

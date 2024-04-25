2dec31025421185039274e0daa77d19b8cf7bc5a
Sergio Gomez <sergio.gomez2000@hotmail.com>
2024-04-25 11:28:04 -0500

    [ADD] Data card creation

--- apps/web/src/app/(dashboard)/user/devices/[deviceName]/page.tsx
+++ apps/web/src/app/(dashboard)/user/devices/[deviceName]/page.tsx
@@ -12,6 +12,9 @@ import { useEffect, useState } from "react";
 import {
   fetchAndReadStreamData,
   filterAndFormatData,
+  getCardsData,
+  getDownloadData,
+  getGraphData,
 } from "utils/dataProcessingUtils";
 
 export default function Device() {
@@ -61,8 +64,10 @@ export default function Device() {
   const consoleData = filteredData?.map((value, index) => {
     return <li key={index}>{value}</li>;
   });
+  const graphData = getGraphData(filteredData);
+  const cardData = getCardsData(filteredData);
   return (
-    <section className="h-full  items-start overflow-y-scroll px-4 pb-20 ">
+    <section className="h-full  items-start overflow-y-scroll px-4 pb-40 ">
       <h2 className="my-0 mb-2 border-none bg-transparent px-0 font-semibold outline-none focus:ring-0 lg:text-4xl">
         {device?.name}
       </h2>
@@ -84,7 +89,31 @@ export default function Device() {
         Esta sección muestra todos los datos recibidos que no se configuraron
         para graficar
       </p>
-      <div className="dark:border-darkTex mb-4 max-h-32 w-full overflow-y-scroll rounded border border-lightText p-2 text-sm">
+      <div className="dark:border-darkTex relative mb-4 max-h-32 w-full overflow-y-scroll rounded border border-lightText p-2 text-sm">
+        <button
+          className="absolute right-1 top-1 border p-2"
+          onClick={() => {
+            const downloadData = getDownloadData(recievedData);
+            const csvBlob = new Blob([downloadData], {
+              type: "text/csv;charset=utf-8",
+            });
+            const url = window.URL.createObjectURL(csvBlob);
+
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = `${deviceId}.csv`;
+
+            // Append the link to the body, click it, and then remove it
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            // Free up the blob URL
+            window.URL.revokeObjectURL(url);
+          }}
+        >
+          Descargar
+        </button>
         <ul>{consoleData}</ul>
       </div>
       <h4 className="mb-2 text-sm lg:text-xl">Últimos datos recibidos</h4>
@@ -92,35 +121,42 @@ export default function Device() {
         Esta sección muestra los datos configurados como “mostrar ultimo”
       </p>
       <div className="mb-4 flex h-auto items-start justify-start gap-4 overflow-x-scroll p-2">
-        <Card
-          className="mx-auto max-w-xs"
-          decoration="top"
-          decorationColor="indigo"
-        >
-          <p className="text-xs lg:text-sm dark:text-dark-tremor-content">
-            Sales
-          </p>
-          <p className="text-2xl font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            $34,743
-          </p>
-        </Card>
-        <Card
-          className="mx-auto max-w-xs"
-          decoration="top"
-          decorationColor="indigo"
-        >
-          <p className="text-xs lg:text-sm dark:text-dark-tremor-content">
-            Sales
-          </p>
-          <p className="text-2xl font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            $34,743
-          </p>
-        </Card>
+        {cardData.length > 0 ? (
+          cardData.map((value, index) => {
+            return (
+              <Card
+                className="mx-auto max-w-xs"
+                decoration="top"
+                decorationColor="indigo"
+                key={index}
+              >
+                <p className="text-xs lg:text-sm dark:text-dark-tremor-content">
+                  {value.title}
+                </p>
+                <p className="text-2xl font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                  {value.data}
+                </p>
+              </Card>
+            );
+          })
+        ) : (
+          <p>No hay datos configurados para mostrar su ultimo valor</p>
+        )}
       </div>
       <h4 className="mb-2 text-sm lg:text-xl">Graficas</h4>
       <p className="mb-2 text-xs text-lightText lg:text-base dark:text-darkText">
         Esta sección muestra los datos configurados como “Graficar”{" "}
       </p>
+      {graphData.jsonResult.length > 0 ? (
+        <LineChart
+          data={graphData.jsonResult}
+          index="index"
+          categories={graphData.variableNames}
+          onValueChange={(v) => console.log(v)}
+        />
+      ) : (
+        <p>No hay datos configurados para graficar</p>
+      )}
     </section>
   );
 }
--- apps/web/src/utils/dataProcessingUtils.ts
+++ apps/web/src/utils/dataProcessingUtils.ts
@@ -2,6 +2,16 @@ import { add } from "lib/features/fileEnqueu/fileEnqueuSlice";
 import { storeInstace } from "../lib/store";
 import { fileEnque } from "types/fileEnqueu";
 
+interface GraphItem {
+  [key: string]: number | string;
+  index: number;
+}
+
+interface CardItem {
+  title: string;
+  data: number;
+}
+
 function createDataBlob(deviceId: string) {
   const data = storeInstace.getState().serialData;
 
@@ -55,4 +65,129 @@ function filterAndFormatData(data: string) {
   return allValues;
 }
 
-export { createDataBlob, fetchAndReadStreamData, filterAndFormatData };
+function getGraphData(data: string[]) {
+  let filteredData = data.filter((data) => data.includes(">"));
+  let variableNames: string[] = [];
+
+  filteredData.forEach((line, index) => {
+    variableNames.push(line.split(":")[0].replace(">", ""));
+  });
+  variableNames = [...new Set(variableNames)];
+  let joinFormatedData = [];
+
+  for (let i = 0; i < filteredData.length; i++) {
+    let group = [];
+    for (let j = 0; j < variableNames.length; j++) {
+      if (i + j < filteredData.length) {
+        group.push(filteredData[i + j]);
+      }
+    }
+    if (group.length === variableNames.length) {
+      joinFormatedData.push(group.join(";"));
+    }
+    i += variableNames.length - 1;
+  }
+
+  let jsonResult: GraphItem[] = [];
+
+  joinFormatedData.forEach((item, index) => {
+    let obj: GraphItem = { index: index + 1 };
+    let pairs = item.split(";");
+    pairs.forEach((pair) => {
+      let match = pair.match(/>([^:]+):(.+)/);
+      if (match && variableNames.includes(match[1])) {
+        obj[match[1]] = Number(match[2]);
+      }
+    });
+    jsonResult.push(obj);
+  });
+
+  return { jsonResult, variableNames };
+}
+
+function getDownloadData(input: string) {
+  let lines = input.split("\n").filter((line) => line.trim() !== "");
+
+  let variableNames: string[] = [];
+  let dataRows: string[] = [];
+
+  let startIndex = lines.findIndex((line) => line.includes(":"));
+
+  if (startIndex === -1) {
+    return "";
+  }
+
+  lines.slice(startIndex).forEach((line, index) => {
+    const parts = line.split(",");
+    if (index === 0) {
+      variableNames = parts.map((part) => {
+        let key = part.split(":")[0].trim();
+        if (key.startsWith(">")) {
+          key = key.substring(1);
+        }
+        if (key.startsWith("<")) {
+          key = key.substring(1);
+        }
+        return key;
+      });
+    }
+    const data = parts.map((part) => part.split(":")[1]);
+    dataRows.push(data.join(","));
+  });
+
+  const data = variableNames.join(",") + "\n" + dataRows.join("\n");
+
+  return data;
+}
+
+function getCardsData(data: string[]) {
+  if (data.length === 0) {
+    return []; // Return an empty array if no data is provided
+  }
+  let filteredData = data.filter((data) => data.includes("<"));
+  let variableNames: string[] = [];
+
+  filteredData.forEach((line) => {
+    variableNames.push(line.split(":")[0].replace("<", ""));
+  });
+  variableNames = [...new Set(variableNames)];
+
+  let joinFormatedData = [];
+
+  for (let i = 0; i < filteredData.length; i++) {
+    let group = [];
+    for (let j = 0; j < variableNames.length; j++) {
+      if (i + j < filteredData.length) {
+        group.push(filteredData[i + j]);
+      }
+    }
+    if (group.length === variableNames.length) {
+      joinFormatedData.push(group.join(";"));
+    }
+    i += variableNames.length - 1;
+  }
+
+  const lastItem = joinFormatedData[joinFormatedData.length - 1];
+  let result: CardItem[] = [];
+
+  const pairs = lastItem.split(";");
+  pairs.forEach((pair) => {
+    const match = pair.match(/<([^:]+):(.+)/);
+    if (match && variableNames.includes(match[1])) {
+      const item: CardItem = {} as CardItem;
+      item.title = match[1].toString();
+      item.data = parseFloat(match[2]);
+      result.push(item);
+    }
+  });
+
+  return result;
+}
+export {
+  createDataBlob,
+  fetchAndReadStreamData,
+  filterAndFormatData,
+  getGraphData,
+  getDownloadData,
+  getCardsData,
+};

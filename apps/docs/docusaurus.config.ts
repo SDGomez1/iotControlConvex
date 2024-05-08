b2d284b8210776ba717c3da22f37abf739e7e299
Sergio Gomez <sergio.gomez2000@hotmail.com>
2024-05-07 22:32:40 -0500

    [FIX] broken links

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: "facebook", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
@@ -59,9 +59,8 @@ const config: Config = {
           type: "docSidebar",
           sidebarId: "tutorialSidebar",
           position: "left",
-          label: "Tutorial",
+          label: "Docs",
         },
-        { to: "/blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/facebook/docusaurus",
           label: "GitHub",

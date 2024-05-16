d616a83022d1a3e9b6fda113f3c660e6e8be58ae
unknown <Sergio Gomez>
2024-05-16 18:45:40 -0500

    [ADD] new graph function

--- apps/web/src/utils/FileProcessingUtils.ts
+++ apps/web/src/utils/FileProcessingUtils.ts
@@ -292,6 +292,61 @@ function getCardsData(data: string[]) {
 
   return result;
 }
+
+function ejex(array: number[]) {
+  var lista = array.map((data, index) => (index % 2 === 0 ? data : null));
+  let filterList = lista.filter((data): data is number => data !== null);
+  return filterList;
+}
+function generarListaNumeros(filterList: number[]) {
+  let min = Math.min(...filterList);
+  let max = Math.max(...filterList);
+  var listaNumeros = [];
+  var st = String(min);
+  var st2 = String(max);
+  if (st.includes(".") || st2.includes(".")) {
+    for (let i = min; i <= max; i += 0.1) {
+      listaNumeros.push(i);
+    }
+    listaNumeros = listaNumeros.map((numero) => parseFloat(numero.toFixed(1)));
+  } else if (
+    st.length == 2 ||
+    (st2.length == 2 && !st.includes(".")) ||
+    !st2.includes(".")
+  ) {
+    for (let i = min; i <= max; i += 1) {
+      listaNumeros.push(i);
+    }
+  } else if (
+    st.length == 3 ||
+    (st2.length == 3 && !st.includes(".")) ||
+    !st2.includes(".")
+  ) {
+    for (let i = min; i <= max; i += 10) {
+      listaNumeros.push(i);
+    }
+  } else if (
+    st.length == 4 ||
+    (st2.length == 4 && !st.includes(".")) ||
+    !st2.includes(".")
+  ) {
+    for (let i = min; i <= max; i += 100) {
+      listaNumeros.push(i);
+    }
+  }
+  var analizar = listaNumeros;
+  var nums = [];
+  for (let pos = 0; pos < filterList.length; pos++) {
+    if (String(filterList[pos]).includes(".")) {
+      nums.push(filterList[pos]);
+    }
+  }
+  analizar.push(...nums);
+  analizar.sort((a, b) => a - b);
+  var listaFinal = analizar;
+  return listaFinal;
+}
+
 export {
   createDataBlob,
   fetchAndReadStreamData,
@@ -299,4 +354,6 @@ export {
   getGraphData,
   getDownloadData,
   getCardsData,
+  ejex,
+  generarListaNumeros,
 };
